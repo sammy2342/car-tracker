ff10ef67e0a3e1df3b0a320142ed398e2e2f379c
sammyfares <sammyfares2003@gmail.com>
2022-10-07 19:43:31 -0400

    complete aouth

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
+
+require('dotenv').config()
+
+require('./config/database')
+require('./config/passport')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const passport = require('passport');
 
 var app = express();
 
@@ -16,8 +23,23 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use( function(req, res, next) {
+  res.locals.user = req.user
+  console.log(res.locals.user)
+  next()
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
